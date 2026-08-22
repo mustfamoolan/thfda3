f364cf4290983ff386b68863c4e250292d4c7e29
mustfamoolan <samiraalyaseen20@gmail.com>
2026-08-23 02:20:35 +0300

    Fix AppBar real-time sync for custom avatar photos, emojis, and player nicknames

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2571227702" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1385309876" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
